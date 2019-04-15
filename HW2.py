fa4bc5882c775f641019bed02d74114bfe391149
Tamir Neeman <tamir.neeman@gmail.com>
2019-04-15 15:51:57 +0300

    basic imputing and cleaning

--- HW2.py
+++ HW2.py
@@ -1,19 +1,53 @@
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
+from sklearn.impute import SimpleImputer
+from sklearn import preprocessing
 
 
 def save_to_csv(name, dataset, sep=','):
     dataset.to_csv(name + ".csv", sep=sep, encoding='utf-8')
 
+
 def data_preperation(name, df):
     print('to complete')
 
+
 def identify_and_set_correct_types(elections_df):
-    features = elections_df.keys().drop('Vote')
+    le = preprocessing.LabelEncoder()
+    features = elections_df.keys()
+
     for feature in features:
         if elections_df[feature].dtype == 'object':
-            elections_df[feature] = elections_df[feature].astype('category')
+            elections_df[feature] = le.fit_transform(elections_df[feature].astype(str))
+
+
+def dummify_categories(elections_df):
+    return pd.get_dummies(elections_df)
+
+
+def impute(X, y):
+    imp = SimpleImputer(missing_values=np.nan, strategy='most_frequent')
+    transformed = imp.fit_transform(X, y)
+    df_transformed = pd.DataFrame(transformed, columns=X.columns).astype(X.dtypes.to_dict())
+    return df_transformed
+
+
+def handle_corrupt_data(elections_df):
+    # -122707.360023781 , -108232.195771866 , 160017.623874907 on AVG_lottary_expanses
+    # -694.383176544183 on Avg_monthly_expense_when_under_age_21
+
+    # elections_errs = elections_df[(elections_df.AVG_lottary_expanses == -122707.360023781) |
+    #                               (elections_df.Avg_monthly_expense_when_under_age_21 == -694.383176544183)]
+    elections_df.loc[
+        elections_df['AVG_lottary_expanses'] == -122707.360023781, ['AVG_lottary_expanses']] = 0
+    elections_df.loc[
+        elections_df['AVG_lottary_expanses'] == -108232.195771866, ['AVG_lottary_expanses']] = 0
+    elections_df.loc[
+        elections_df['AVG_lottary_expanses'] == -160017.623874907, ['AVG_lottary_expanses']] = 0
+    elections_df.loc[
+        elections_df['Avg_monthly_expense_when_under_age_21'] == -694.383176544183, [
+            'Avg_monthly_expense_when_under_age_21']] = 0
 
 
 def main():
@@ -22,16 +56,30 @@ def main():
     # df.dtypes to watch the columns types
 
     # Split the data - 60% train, 20% validation, 20% test
-    train_raw, validate_raw, test_raw = np.split(elections_df.sample(frac=1), [int(.6 * len(elections_df)),
-                                                                     int(.8 * len(elections_df))])
-    identify_and_set_correct_types(elections_df)
+    train_df, validate_df, test_df = np.split(elections_df.sample(frac=1), [int(.6 * len(elections_df)),
+                                                                            int(.8 * len(elections_df))])
 
-    train, validate, test = np.split(elections_df.sample(frac=1), [int(.6 * len(elections_df)),
-                                                                   int(.8 * len(elections_df))])
-    for dataset in [['train_raw', train_raw], ['validate_raw', validate_raw], ['test_raw', test_raw]]:
+    for dataset in [['train_raw', train_df], ['validate_raw', validate_df], ['test_raw', test_df]]:
         save_to_csv(name=dataset[0], dataset=dataset[1])
-    for dataset in [['train', train], ['validate', validate], ['test', test]]:
-        data_preperation(name=dataset[0], df=dataset[1])
+
+    train_y_df = train_df.filter(["Vote"])
+    train_df = train_df.drop(columns=["Vote"])
+
+    train_df = impute(train_df, train_y_df)
+
+    print("imputed")
+    train_df = dummify_categories(train_df)
+    print("dummied")
+
+    # for name, df in [['train', train], ['validate', validate], ['test', test]]:
+    #     train = identify_and_set_correct_types(df)
+
+    for dataset in [['train_manipulated', train_df]]:
+        save_to_csv(name=dataset[0], dataset=dataset[1])
+
+    # for name, df in [['train', train_df], ['validate', validate_df], ['test', test_df]]:
+    #     data_preperation(name=name, df=df)
+
 
 if __name__ == "__main__":
     main()
