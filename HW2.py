f5ed0f6b9347b814b7098e7f735e5d5d3d891ba8
RonP3 <ron.paz@gmail.com>
2019-04-26 11:18:14 +0300

    Scaling

--- HW2.py
+++ HW2.py
@@ -10,6 +10,28 @@ from sequential_selection import sfs, sbs, bds
 import pickle
 
 
+# feature distribution
+normal_dist_features = ['Political_interest_Total_Score', 'Number_of_valued_Kneset_members',
+                        'Number_of_differnt_parties_voted_for', 'Avg_size_per_room',
+                        'Avg_Satisfaction_with_previous_vote', 'Avg_monthly_household_cost',
+                        'Avg_monthly_expense_on_pets_or_plants', 'Avg_government_satisfaction']
+
+uniform_dist_features = ['Yearly_IncomeK', 'Yearly_ExpensesK', 'Overall_happiness_score', 'Occupation',
+                         'Occupation_Satisfaction', 'Main_transportation', 'Looking_at_poles_results', 'Gender',
+                         'Garden_sqr_meter_per_person_in_residancy_area', 'Financial_balance_score_(0-1)',
+                         'Financial_agenda_matters', 'Age_group', '%Time_invested_in_work', '%Of_Household_Income',
+                         '%_satisfaction_financial_policy']
+
+other_dist_features = ['Will_vote_only_large_party', 'Voting_Time', 'Phone_minutes_10_years',
+                       'Num_of_kids_born_last_10_years', 'Most_Important_Issue', 'Married', 'Last_school_grades',
+                       'Avg_Residancy_Altitude', 'Avg_monthly_income_all_years',
+                       'Avg_monthly_expense_when_under_age_21', 'AVG_lottary_expanses', 'Avg_environmental_importance',
+                       'Avg_education_importance', 'Weighted_education_rank']
+
+categorical_features = ['Most_Important_Issue', 'Looking_at_poles_results', 'Married', 'Gender', 'Voting_Time',
+                        'Will_vote_only_large_party', 'Age_group', 'Main_transportation', 'Occupation',
+                        'Financial_agenda_matters']
+
 def save_to_csv(name, dataset, sep=','):
     dataset.to_csv(name + ".csv", sep=sep, encoding='utf-8')
 
@@ -51,6 +73,25 @@ def handle_corrupt_data(elections_df):
             'Avg_monthly_expense_when_under_age_21']] = np.nan
 
 
+def scale(df):
+    # uniform dist scale
+    uniform_features = df[list(set(uniform_dist_features)-set(categorical_features))]
+    df[uniform_features.keys()] = (preprocessing.MinMaxScaler(feature_range=(-1, 1),
+                                                              copy=False).fit_transform(uniform_features))
+
+    # normal dist scale
+    normal_features = df[list(set(normal_dist_features)-set(categorical_features))]
+    df[normal_features.keys()] = (df[normal_features.keys()] -
+                                  df[normal_features.keys()].mean())/df[normal_features.keys()].std()
+    df[normal_features.keys()] = (preprocessing.MinMaxScaler(feature_range=(-1, 1),
+                                                              copy=False).fit_transform(normal_features))
+    # other dist scale
+    other_features = df[list(set(other_dist_features) - set(categorical_features))]
+    df[other_features.keys()] = (preprocessing.MinMaxScaler(feature_range=(-1, 1),
+                                                              copy=False).fit_transform(other_features))
+    return df
+
+
 def main():
     # Data loading
     elections_df = pd.read_csv('ElectionsData.csv')
@@ -72,6 +113,9 @@ def main():
     train_df = dummify_categories(train_df)
     print("dummied")
 
+    # train_df = scale(train_df)
+    # print("scaled")
+
     #  manipulate validation set
     validate_y_df = validate_df.filter(["Vote"])
     validate_df = validate_df.drop(columns=["Vote"])
@@ -81,6 +125,9 @@ def main():
 
     validate_df = dummify_categories(validate_df)
     print("dummied")
+
+    # train_df = scale(train_df)
+    # print("scaled")
 
     selected = bds(train_df, np.ravel(train_y_df), validate_df, np.ravel(validate_y_df), tree.DecisionTreeClassifier())
     print("selected: ", selected)
