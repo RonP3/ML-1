3cbc1d3a99dc9e3e1c9f31a9b35aa699eb8387ce
Tamir Neeman <tamir.neeman@gmail.com>
2019-04-27 00:05:25 +0300

    major refactor

--- HW2.py
+++ HW2.py
@@ -33,145 +33,177 @@ categorical_features = ['Most_Important_Issue', 'Looking_at_poles_results', 'Mar
                         'Financial_agenda_matters']
 
 
+class DataPreparator:
+
+    def __init__(self, df, target):
+        self.df = df
+        self.target = target
+        self.train_df, self.validate_df, self.test_df = np.split(self.df.sample(frac=1), [int(.6 * len(self.df)),
+                                                                                          int(.8 * len(self.df))])
+
+        self.train_y_df = self.train_df.filter([self.target])
+        self.validate_y_df = self.validate_df.filter([self.target])
+        self.test_y_df = self.test_df.filter([self.target])
+
+    def save_to_file(self, suffix=''):
+        for dataset in [['train_' + suffix, self.train_df], ['validate_' + suffix, self.validate_df],
+                        ['test_' + suffix, self.test_df]]:
+            save_to_csv(name=dataset[0], dataset=dataset[1])
+
+    def process_data_for_df(self, X):
+        y = X.filter([self.target])
+        X = X.drop(columns=[self.target])
+        X = self.handle_negative_data(X)
+        X = self.impute(X, y)
+        X = self.dummify_categories(X)
+        X = self.scale(X)
+        return X, y
+
+    def update_selected_features(self, selected):
+        selected = set(selected).intersection(set(self.train_df.keys())).intersection(
+            set(self.validate_df.keys()).intersection(set(self.test_df.keys())))
+
+        self.train_df = self.train_df[selected]
+        self.validate_df = self.validate_df[selected]
+        self.test_df = self.test_df[selected]
+
+    def process_data(self):
+        self.train_df, self.train_y_df = self.process_data_for_df(self.train_df)
+        self.validate_df, self.validate_y_df = self.process_data_for_df(self.validate_df)
+        self.test_df, self.test_y_df = self.process_data_for_df(self.test_df)
+
+    def identify_and_set_correct_types(self):
+        features = set(self.train_df.keys()) - {self.target}
+
+        numerical, categorical = [], []
+        for feature in features:
+            if self.train_df[feature].dtype == 'float' or self.train_df[feature].dtype == 'int':
+                numerical.append(feature)
+            else:
+                categorical.append(feature)
+        return numerical, categorical
+
+    def impute(self, X, y):
+        # todo: remember, when imputing test- need to impute by means or most frequent of *train*, not *test*!
+        # todo: we still need to change this!
+        numerical, _ = self.identify_and_set_correct_types()
+        numerical = set(numerical).intersection(set(X.keys()))
+        categorical = set(X.keys()) - set(numerical)
+        X_categorical = X[categorical]
+        imp = SimpleImputer(missing_values=np.nan, strategy='most_frequent')
+        transformed = imp.fit_transform(X_categorical, y)
+        categorical_transformed = pd.DataFrame(transformed, columns=X_categorical.columns).astype(
+            X_categorical.dtypes.to_dict())
+
+        X_numerical = X[numerical]
+        imp = SimpleImputer(missing_values=np.nan, strategy='mean')
+        transformed = imp.fit_transform(X_numerical, y)
+        numerical_transformed = pd.DataFrame(transformed, columns=X_numerical.columns).astype(
+            X_numerical.dtypes.to_dict())
+
+        return pd.concat([categorical_transformed, numerical_transformed], axis=1)
+
+    def dummify_categories(self, X):
+        return pd.get_dummies(X)
+
+    def handle_corrupt_data(self):
+        self.df.loc[
+            self.df < 0] = np.nan
+
+    def scale(self, df):
+        # uniform dist scale
+        numerical, categorical = self.identify_and_set_correct_types()
+        numerical = set(numerical).intersection(set(df.keys()))
+
+        uniform_features = df[list(set(uniform_dist_features).intersection(numerical))]
+        df[uniform_features.keys()] = (preprocessing.MinMaxScaler(feature_range=(-1, 1),
+                                                                  copy=False).fit_transform(uniform_features))
+
+        # normal dist scale
+        normal_features = df[
+            list(set(normal_dist_features).union(set(other_dist_features)).intersection(numerical))]
+        df[normal_features.keys()] = (df[normal_features.keys()] -
+                                      df[normal_features.keys()].mean()) / df[normal_features.keys()].std()
+        return df
+
+    def handle_negative_data(self, X):
+        numeric_columns = X.select_dtypes([np.float64]).columns.data.obj
+
+        for column in numeric_columns:
+            X.loc[X[column] < 0, column] = np.nan
+        return X
+
+    def relief(self, save=False, load=False, test=False, iterates=5, threshold_type='best', threshold=30):
+        if load:
+            output = open('relief.pkl', 'wb')
+            selected = pickle.load(output)
+            output.close()
+            return selected
+
+        selected = relief(self.train_df, self.train_y_df, iterates, threshold_type, threshold)
+        if test:
+            for classifier in {tree.DecisionTreeClassifier, GaussianNB, SVC, KNN}:
+                clf = classifier()
+                clf.fit(self.train_df, np.ravel(self.train_y_df))
+                print(str(classifier), " before Relief: ", clf.score(self.validate_df, self.validate_y_df))
+                clf.fit(self.train_df[selected], np.ravel(self.train_y_df))
+                print(str(classifier), " after Relief:", len(selected), " ",
+                      clf.score(self.validate_df[selected], self.validate_y_df))
+
+        if save:
+            output = open('relief.pkl', 'wb')
+            pickle.dump(selected, output)
+            output.close()
+
+        return selected
+
+    def sfs(self, save=False, load=False, test=False, classifier=SVC(gamma='auto')):
+
+        if load:
+            output = open('sfs.pkl', 'wb')
+            selected = pickle.load(output)
+            output.close()
+            return selected
+
+        selected = sfs(self.train_df, np.ravel(self.train_y_df), self.validate_df, np.ravel(self.validate_y_df),
+                       classifier)
+        if test:
+            for classifier in {tree.DecisionTreeClassifier, GaussianNB, SVC, KNN}:
+                clf = classifier()
+                clf.fit(self.train_df, np.ravel(self.train_y_df))
+                print(str(classifier), " before sfs: ", clf.score(self.validate_df, self.validate_y_df))
+                clf.fit(self.train_df[selected], np.ravel(self.train_y_df))
+                print(str(classifier), " after sfs:", len(selected), " ",
+                      clf.score(self.validate_df[selected], self.validate_y_df))
+
+        if save:
+            output = open('sfs.pkl', 'wb')
+            pickle.dump(selected, output)
+            output.close()
+
+        return selected
+
+
 def save_to_csv(name, dataset, sep=','):
     dataset.to_csv(name + ".csv", sep=sep, encoding='utf-8')
 
 
-def data_preperation(name, df):
-    print('to complete')
-
-
-def identify_and_set_correct_types(elections_df):
-    le = preprocessing.LabelEncoder()
-    features = elections_df.keys()
-
-    for feature in features:
-        if elections_df[feature].dtype == 'object':
-            elections_df[feature] = le.fit_transform(elections_df[feature].astype(str))
-
-
-def dummify_categories(elections_df):
-    return pd.get_dummies(elections_df)
-
-
-def impute(X, y):
-    imp = SimpleImputer(missing_values=np.nan, strategy='most_frequent')
-    transformed = imp.fit_transform(X, y)
-    df_transformed = pd.DataFrame(transformed, columns=X.columns).astype(X.dtypes.to_dict())
-    return df_transformed
-
-
-def handle_negative_data(df):
-    numeric_columns = df.select_dtypes([np.float64]).columns.data.obj
-    for column in numeric_columns:
-        df.loc[df[column] < 0, column] = np.nan
-    return df
-
-
-def scale(df):
-    # uniform dist scale
-    uniform_features = df[list(set(uniform_dist_features) - set(categorical_features))]
-    df[uniform_features.keys()] = (preprocessing.MinMaxScaler(feature_range=(-1, 1),
-                                                              copy=False).fit_transform(uniform_features))
-
-    # normal dist scale
-    normal_features = df[list(set(normal_dist_features).union(set(other_dist_features)) - set(categorical_features))]
-    df[normal_features.keys()] = (df[normal_features.keys()] -
-                                  df[normal_features.keys()].mean()) / df[normal_features.keys()].std()
-    # df[normal_features.keys()] = (preprocessing.MinMaxScaler(feature_range=(-1, 1),
-    #                                                           copy=False).fit_transform(normal_features))
-
-    # other dist scale
-    # other_features = df[list(set(other_dist_features) - set(categorical_features))]
-    # df[other_features.keys()] = (preprocessing.MinMaxScaler(feature_range=(-1, 1),
-    #                                                           copy=False).fit_transform(other_features))
-    return df
-
-
 def main():
     elections_df = pd.read_csv('ElectionsData.csv')
-    elections_df = handle_negative_data(elections_df)
-
-    # Split the data - 60% train, 20% validation, 20% test
-    train_df, validate_df, test_df = np.split(elections_df.sample(frac=1), [int(.6 * len(elections_df)),
-                                                                            int(.8 * len(elections_df))])
-
-    for dataset in [['train_raw', train_df], ['validate_raw', validate_df], ['test_raw', test_df]]:
-        save_to_csv(name=dataset[0], dataset=dataset[1])
-
-    train_y_df = train_df.filter(["Vote"])
-    train_df = train_df.drop(columns=["Vote"])
-
-    train_df = impute(train_df, train_y_df)
-    print("imputed")
-
-    train_df = dummify_categories(train_df)
-    print("dummied")
-
-    train_df = scale(train_df)
-    print("scaled")
-
-    #  manipulate validation set
-    validate_y_df = validate_df.filter(["Vote"])
-    validate_df = validate_df.drop(columns=["Vote"])
-
-    validate_df = impute(validate_df, validate_y_df)
-    print("validate imputed")
-
-    validate_df = dummify_categories(validate_df)
-    print("validate dummied")
-
-    validate_df = scale(validate_df)
-    print("validate scaled")
-
-    selected = relief(train_df, train_y_df, 1, 'best', 30)
-
-    for classifier in {tree.DecisionTreeClassifier, GaussianNB, SVC, KNN}:
-        clf = classifier()
-        clf.fit(train_df, np.ravel(train_y_df))
-        print(str(classifier), " before Relief: ", clf.score(validate_df, validate_y_df))
-        clf.fit(train_df[selected], np.ravel(train_y_df))
-        print(str(classifier), " after Relief:", len(selected), " ", clf.score(validate_df[selected], validate_y_df))
-
-    output = open('relief.pkl', 'wb')
-    pickle.dump(selected, output)
-    output.close()
-
-    # output = open('relief.pkl', 'rb')
-    # selected = pickle.load(output)
-    # output.close()
-
-    train_df = train_df[selected]
-    validate_df = validate_df[selected]
-
-    selected = bds(train_df, np.ravel(train_y_df), validate_df, np.ravel(validate_y_df), SVC(gamma='auto'))
-
-    for classifier in {tree.DecisionTreeClassifier, GaussianNB, SVC, KNN}:
-        clf = classifier()
-        clf.fit(train_df, np.ravel(train_y_df))
-        print(str(classifier), " before sfs: ", clf.score(validate_df, validate_y_df))
-        clf.fit(train_df[selected], np.ravel(train_y_df))
-        print(str(classifier), " after sfs:", len(selected), " ", clf.score(validate_df[selected], validate_y_df))
-
-    train_df = train_df[selected]
-    validate_df = validate_df[selected]
-
-    output = open('sfs.pkl', 'wb')
-    pickle.dump(selected, output)
-    output.close()
-
-    # output = open('sfs.pkl', 'wb')
-    # selected = pickle.load(output)
-    # output.close()
-
-    # for name, df in [['train', train], ['validate', validate], ['test', test]]:
-    #     train = identify_and_set_correct_types(df)
-    #
-    for dataset in [['train_manipulated', train_df]]:
-        save_to_csv(name=dataset[0], dataset=dataset[1])
-
-    for name, df in [['train', train_df], ['validate', validate_df], ['test', test_df]]:
-        data_preperation(name=name, df=df)
+    dp = DataPreparator(elections_df, 'Vote')
+    print("saving raw data")
+    dp.save_to_file('raw')
+    dp.process_data()
+    print("finished processing")
+    print("starting relief")
+    selected = dp.relief(save=True, load=False, test=True, iterates=1000, threshold_type='best', threshold=30)
+    print("finished relief")
+    dp.update_selected_features(selected)
+    print("starting sfs")
+    selected = dp.sfs(save=True, load=False, test=True, classifier=SVC())
+    print("finished sfs")
+    dp.update_selected_features(selected)
+    dp.save_to_file('processed')
 
 
 if __name__ == "__main__":
